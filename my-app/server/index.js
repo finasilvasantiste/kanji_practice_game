9cb04273019b1c14f10ed37526342412d327c9a8
Fina Silva-Santisteban <fina.santiste@gmail.com>
2019-07-25 14:23:39 -0700

    Add function to read csv.

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
+const fs = require('fs');
 
+// const fs = require('fs').promises;
 const app = express();
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
@@ -12,6 +16,20 @@ app.get('/api/greeting', (req, res) => {
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
-app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
+app.listen(3001, () => {
+    console.log('Express server is running on localhost:3001');
+    readCSV();
+  }
 );
+
+function readCSV(){
+  const fileName = './server/data.csv';
+  fs.readFile(fileName, 'utf8', function (err, data) {
+    if (err){
+      return console.log(err);
+    }else{
+      console.log('CSV read: ');
+      console.log(data);
+    }
+  });
+}
