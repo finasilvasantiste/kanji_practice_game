b92ed2d6025bf6c92fe8a870dc9d008a2efff2b2
Fina Silva-Santisteban <fina.santiste@gmail.com>
2019-07-27 14:54:37 -0700

    Add shuffle logic.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,20 @@
 import React, {Component} from 'react';
 import './App.css';
 import FlashCard from './FlashCard';
+import shuffle from './shuffle_logic';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null
+      data: null,
+      order: null
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
     const that = this;
+
     fetch('/data').then((response) => {
       const decoder = new TextDecoder();
       const reader = response.body.getReader();
@@ -23,10 +24,12 @@ class App extends Component {
       reader.read().then(function processResult(result) {
         if (result.done) return;
         const data = JSON.parse(decoder.decode(result.value, {stream: true}));
-        console.log(data);
 
         // Set state with data from csv
-        that.setState({data : data});
+        that.setState({
+          data : data,
+          order : that.setFlashCardOrder(data.length)
+          });
 
         // Read some more, and recall this function
         return reader.read().then(processResult);
@@ -34,29 +37,22 @@ class App extends Component {
     });
   }
 
-  handleChange(event) {
-    this.setState({ name: event.target.value });
-  }
+  setFlashCardOrder(dataLength){
+    console.log('setFlashCardOrder');
+    console.log(dataLength);
+    const lengthArray = Array.from(Array(dataLength).keys());
+    console.log(shuffle(lengthArray));
 
-  handleSubmit(event) {
-    event.preventDefault();
-    console.log(this.state.data);
-    // fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
-    //   .then(response => response.json())
-    //   .then(response => {
-    //         console.log(response);
-    //         });
-      // .then(state => this.setState(state));
   }
 
   render() {
-    console.log('render');
-    console.log(this.state.data ? this.state.data[0] : this.state.data);
+    // console.log('render');
+    // console.log(this.state.data ? this.state.data[0] : this.state.data);
 
     return (
       <div className="App">
         <div>
-          <FlashCard kanji="(kanji)" meaning="(meaning)" seen="(false)"/>
+          <FlashCard kanji="(kanji)" meaning="(meaning)" seen="true"/>
         </div>
       </div>
     );
--- my-app/src/FlashCard.js
+++ my-app/src/FlashCard.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 
-
 class FlashCard extends Component{
   constructor(props) {
     super(props);
@@ -11,20 +10,26 @@ class FlashCard extends Component{
     };
   }
 
+  componentDidMount() {
+    this.setState({
+      kanji: this.props.kanji,
+      meaning: this.props.meaning,
+      seen: this.props.seen
+    })
+  }
 
   render() {
     const kanji = this.state.kanji;
     const meaning = this.state.meaning;
     const seen = this.state.seen;
 
-    console.log(kanji, meaning, seen);
+    // console.log(kanji, meaning, seen);
 
     return (
-      <div>
-        <span>This will be a FlashCard.</span>
-      <span>{kanji}</span>
-      <span>{meaning}</span>
-      <span>{seen ? 'True' : 'False'}</span>
+      <div className={'flash_card'}>
+      <span className={'kanji'}>{kanji}</span>
+      <span className={'meaning'}>{meaning}</span>
+      <span className={'seen'}>{seen ? 'True' : 'False'}</span>
       </div>
     );
   }
