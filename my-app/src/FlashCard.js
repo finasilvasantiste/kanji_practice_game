ec6e2080c38f738d7c117d5ca6b17f62eaaf64c5
Fina Silva-Santisteban <fina.santiste@gmail.com>
2019-07-28 12:12:58 -0700

    Add show meaning button.

--- my-app/src/FlashCard.js
+++ my-app/src/FlashCard.js
@@ -1,20 +1,46 @@
 import React, {Component} from 'react';
+import Button from "react-bootstrap/Button";
 
 class FlashCard extends Component{
   constructor(props) {
     super(props);
+
     this.state = {
       kanji: null,
-      meaning: null
+      meaning: null,
+      showMeaning : false,
     };
+
+    this.handleButtonShowMeaning = this.handleButtonShowMeaning.bind(this);
+  }
+
+  handleButtonShowMeaning(){
+    let visibility = !this.state.showMeaning;
+
+    this.setState({
+      showMeaning: visibility
+    });
+  }
+
+  componentWillReceiveProps(nextProps){
+    this.setState({
+      kanji: nextProps.kanji,
+      meaning: nextProps.meaning,
+      showMeaning: false,
+    })
   }
 
   render() {
+    const visibility = this.state.showMeaning ? 'Hide' : 'Show';
 
     return (
       <div className="flashCard">
       <span className="kanji">{this.props.kanji}</span>
-      <span className="meaning">{this.props.meaning}</span>
+        {this.state.showMeaning
+        ? <span className="meaning">{this.props.meaning}</span>
+        : null
+        }
+      <Button variant="light" onClick={this.handleButtonShowMeaning}>{visibility} meaning</Button>
       </div>
     );
   }
